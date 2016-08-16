e3b50e52c87e083d4e2ee6ceeebeb68ea59bf293
Dave Kuntz <dkuntz811@gmail.com>
2016-08-16 10:06:04 -0500

    Fixed error in #8

--- solution.sql
+++ solution.sql
@@ -17,7 +17,8 @@ SELECT username, account_balance FROM syntax_practice ORDER BY account_balance A
 --7. Get all users with account balances that are more than $100.
 SELECT * FROM syntax_practice WHERE account_balance > 100.00;
 --8. Add a new record. --HAVE NO CLUE WHY THIS IS INCORRECT BELOW:
-INSERT INTO syntax_practice VALUES ('bob', 'denver', 0, 32, 450000.00);
+INSERT INTO syntax_practice (username, city, transactions_completed, transactions_attempted, account_balance)
+VALUES ('bob', 'denver', 0, 32, 450000.00);
 --9. The bank is losing money in Miami and Phoenix and needs to unload low transaction customers:
 --Delete users that reside in miami OR phoenix and have completed fewer than 5 transactions.
 DELETE FROM syntax_practice WHERE (city = 'miami' OR city = 'phoenix') AND transactions_completed < 5;
